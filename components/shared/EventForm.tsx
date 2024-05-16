7f4ee6d7588e9ddffd898ded1e8c2c3f42308ec9
ABOU JNEID Crystal-Monica <brt.cmaboujneid@cma-cgm.com>
2024-05-16 22:10:52 +0300

    rename roote folder

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+type EventFormProps = {
+    userId: string;
+    type: 'Create' | 'Update'
+}
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  return (
+    <div>EventForm {type}</div>
+  )
+}
+
+export default EventForm
